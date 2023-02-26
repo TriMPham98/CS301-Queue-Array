7d9c10e845dc57b65af9d53a7cfd2c8ea91a0e9f
Tri <trimpham98@gmail.com>
2023-02-26 00:25:20 -0800

    Recommit deleted files

--- Queue.cpp
+++ Queue.cpp
@@ -23,43 +23,64 @@ Queue<T>::Queue(int max)
     numItems = 0;
     maxQue = max;
     front = 0;
-    rear = maxQue -1;
+    rear = maxQue - 1;
+    items = new T[maxQue];
 }
 
 template<class T>
 Queue<T>::~Queue()         // Class destructor
 {
+    delete[] items;
 }
 
 template<class T>
 void Queue<T>::MakeEmpty()
 // Post: front and rear have been reset to the empty state.
 {
+    front = 0;
+    rear = maxQue - 1;
 }
 
 template<class T>
 bool Queue<T>::IsEmpty() const
 // Returns true if the queue is empty; false otherwise.
 {
+    return numItems == 0;
 }
 
 template<class T>
 bool Queue<T>::IsFull() const
 // Returns true if the queue is full; false otherwise.
 {
+    return numItems == maxQue;
 }
 
 template<class T>
 void Queue<T>::Enqueue(T newItem)
 // Post: If (queue is not full) newItem is at the rear of the queue;
-//       otherwise a FullQueue exception is thrown.  
+//       otherwise a FullQueue exception is thrown.
 {
+    if (IsFull()) {
+        throw FullQueue();
+    }
+    rear = (rear + 1) % maxQue;
+    items[rear] = newItem;
+    numItems++;
 }
 
 template<class T>
 T Queue<T>::Dequeue()
-// Post: If (queue is not empty) the front of the queue has been 
-//       removed and a copy returned in item; 
+// Post: If (queue is not empty) the front of the queue has been
+//       removed and a copy returned in item;
 //       otherwise a EmptyQueue exception has been thrown.
 {
+    if (IsEmpty()) {
+        throw EmptyQueue();
+    }
+
+    T dequeuedItem = items[front];
+    front = (front + 1) % maxQue;
+    numItems--;
+
+    return dequeuedItem;
 }
